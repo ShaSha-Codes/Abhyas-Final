b0f95f70273db80daf5584cf6144d5106eca3a25
Mihir <mihirtayshete@gmail.com>
2023-04-07 00:23:25 +0530

    improved ui for Lectures page

--- abhyas/src/pages/Lecture.jsx
+++ abhyas/src/pages/Lecture.jsx
@@ -7,12 +7,24 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import { Typography } from '@mui/material'
 import useLogout from '../hooks/useLogout'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 
 const Lecture = () => {
   const {lecCode} = useParams()
   const [lecture,setLecture] = React.useState(null)
+  const [show,setShow] = React.useState(false)
+  const [buttonContent,setButtonContent] = React.useState("Show More")
   const checker=useLogout()
 
+  const setShowValue=()=>{
+    setShow(!show)
+    if(show){
+      setButtonContent("Show More")
+    }else{
+      setButtonContent("Show Less")
+    }
+  }
 
   React.useEffect(()=>{
     checker("Lectures",lecCode)
@@ -45,30 +57,27 @@ const Lecture = () => {
 
  
         { lecture!==null &&
-           <Grid xs={8}>
-            <video  width="750" height="500" controls>
+           <Grid sx={{backgroundColor:"black", borderRadius:2}} xs={12}  display="flex" direction="column" alignItems="center"  justifyContent="center" >
+            <video style={{borderRadius:'5px'}} width= "75%" controls >
               <source src={lecture?.file}  />
             </video>
             </Grid>
         }
         {
-          <Grid xs={4}>
-          <Paper sx={{p:5,minHeight:'300px'}}>
-            <Typography variant="h5" component="h1" gutterBottom>
-              Title:
-            </Typography>
-            <hr/>
-            <Typography sx={{mb:5}} variant="h6" component="h2" gutterBottom>
-              {lecture?.title}
-            </Typography>
-            <Typography variant="h5" component="h2" gutterBottom>
-              Descrption:
-            </Typography>
-            <hr/>
-            <Typography variant="h6" component="h3" gutterBottom>
-              {lecture?.description}
-            </Typography>
-         
+          <Grid xs={12} >
+          <Paper sx={{marginTop:1,p:1.5,paddingLeft:10,minHeight:'10px',backgroundColor:'#f2f2f2',borderRadius:2}}>
+            <div style={{display:"flex", style:"row" , justifyContent:"space-between" }}>
+            <Typography variant="h5" sx={{fontWeight:'bold',color:'black'}}>{lecture?.title}</Typography>
+            <Typography sx={{color:'gray', fontSize:'15px', marginTop:'auto'}}><button onClick={setShowValue} style={{display:'flex', alignItems:'center',borderRadius:"10px",border:'solid 0.5px gray'}}>{buttonContent}{!show&& <ArrowDropDownIcon/>}{show&&<ArrowDropUpIcon/>}</button></Typography>
+            </div>
+             
+             {show && 
+             <div style={{marginTop:'10px'}}>
+              <hr style={{borderTop:'1px dotted gray'}}/>
+              <p>Description:</p>
+             <Typography variant="body1" sx={{color:'black',marginTop:-1}}>{lecture?.description}</Typography>
+             </div>} 
+            
      
           </Paper>
           </Grid>
